e9ef1404611b892076fdb8ae920a3d47c67431b8
iZaaKGabrieL <isaccgabriel56@gmail.com>
2024-04-23 20:18:38 -0300

    aula de js continua amanha 24/04

---
+++ CURSO DE JAVASCRIPT/arquivo.js
@@ -0,0 +1,12 @@
+function somando(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+function realParaDolar (real, dolar){
+    return real * dolar;
+}
+
+var total = realParaDolar(5.00, 5.13);
+
+alert("o valor atual do dolar e de R$" + total);
+
