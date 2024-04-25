0cb601389be27428bf1e4b8ba1599111a59b31de
iZaaKGabrieL <isaccgabriel56@gmail.com>
2024-04-25 16:32:26 -0300

    aula 9  de js sobre objetos

---
+++ CURSO DE JAVASCRIPT/arquivo,.js
@@ -0,0 +1,15 @@
+/* OBJETOS SÃO VARIAVEIS COM VARIOS VALORES DENTRO DELAS, DENTRO DOS OBJETOS TAMBEM EXISTEM FUNÇÕ~ES QUE TAMBEM SAO CONHECIDAS COMO METODOS. */
+
+let carro = 
+{marca: "fiat",
+modelo: "palio", 
+ano: 2013,
+placa: "ABC-123", 
+buzina: function() {
+    alert("biiiiiiiiiiiiiiiiiiii");
+}
+, completo: function(){
+    return "A marca e  " + this.marca + " e o modelo e " + this.modelo
+}
+}
+console.log(carro.completo());
