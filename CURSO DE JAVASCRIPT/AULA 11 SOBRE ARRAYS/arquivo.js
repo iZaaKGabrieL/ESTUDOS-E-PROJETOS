f3dd2f1e83b26e34ca4514ff8a0993137a9f6077
iZaaKGabrieL <isaccgabriel56@gmail.com>
2024-05-06 15:58:06 -0300

    ATUALIZAÇÃO DA AULA SOBRE ARRAYS.

--- CURSO DE JAVASCRIPT/AULA 11 SOBRE ARRAYS/arquivo.js
+++ CURSO DE JAVASCRIPT/AULA 11 SOBRE ARRAYS/arquivo.js
@@ -1,2 +1,11 @@
+/* Arrays que tem como valor variaveis comuns, para pegar apenas um dos seus valores usamos apenas "pessoa[posição do valor]", agora quando usamos Arrays que em seu valor temos objetos, para pega-los usamos "pessoa.nome do objeto aqui;" Ah e tem mais, Matrizes e Arrays são as mesmas coisas :D .
+*/
+
+
 const lista = ["arroz", "feijão", "carne", "verdura"];
-alert(lista[2]);
+// Array com String abaixo
+ const pessoa = ["Isaac ", "Gabriel ", 18]
+ // array com objeto abaixo:
+ const people = {nome: "isaac ",  sobrenome: "Gabriel ", idade: "18"}
+ alert(lista.length);
+ alert(Array.isArray(pessoa));
