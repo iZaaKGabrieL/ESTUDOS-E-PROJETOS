27df6bbedc35ef51a0d860ffbf7e6c6f713af560
iZaaKGabrieL <isaccgabriel56@gmail.com>
2024-05-08 19:08:51 -0300

    AULA SOBRE ARRAYS FINALIZADA

--- CURSO DE JAVASCRIPT/AULA 11 SOBRE ARRAYS/arquivo.js
+++ CURSO DE JAVASCRIPT/AULA 11 SOBRE ARRAYS/arquivo.js
@@ -1,4 +1,8 @@
 /* Arrays que tem como valor variaveis comuns, para pegar apenas um dos seus valores usamos apenas "pessoa[posição do valor]", agora quando usamos Arrays que em seu valor temos objetos, para pega-los usamos "pessoa.nome do objeto aqui;" Ah e tem mais, Matrizes e Arrays são as mesmas coisas :D .
+
+SOBRE METODOS NO JS:
+
+METODOS SÃO BASICAMENTE FUNÇOES QUE ESTÃO QUASE PRONTAS E QUE SERVE PARA TRABALHARMOS COM NOSSAS ARRAYS.
 */
 
 
