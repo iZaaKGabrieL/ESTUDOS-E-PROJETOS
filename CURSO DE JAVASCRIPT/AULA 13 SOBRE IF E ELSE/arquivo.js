f1119199b9f4310fb334c0da6d300dca4753fca6
iZaaK <isaccgabriel110@gmail.com>
2024-05-13 20:16:16 -0300

    COMEÇO DA AULA 13 SOBRE IF E ELSE

---
+++ CURSO DE JAVASCRIPT/AULA 13 SOBRE IF E ELSE/arquivo.js
@@ -0,0 +1,2 @@
+/*if e else são usados quando queremos que algo acontece apenas quando determinada coisa acontecer no caso usariamos o IF, caso a determinada coisa não aconteça, usamos o ELSE . */
+
