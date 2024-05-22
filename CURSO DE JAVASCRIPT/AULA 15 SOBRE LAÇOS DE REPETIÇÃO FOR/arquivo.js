c55add3e0746a5807215244ce79696859856a727
iZaaK <isaccgabriel110@gmail.com>
2024-05-22 20:10:12 -0300

    AULA 16 SOBRE EVENTOS DE TEMPO JS

--- CURSO DE JAVASCRIPT/AULA 15 SOBRE LAÇOS DE REPETIÇÃO FOR/arquivo.js
+++ CURSO DE JAVASCRIPT/AULA 15 SOBRE LAÇOS DE REPETIÇÃO FOR/arquivo.js
@@ -4,5 +4,5 @@
 var ano = new Date().getFullYear();
 
 for (let i = ano; i >= 1900; i--){
-    document.getElementById("ano").innerHTML += "<option value= '"+i+"'>"+i+"</option>"
+    document.getElementById("ano").innerHTML += "<option value= '"+i+"'>"+i+"</option>";
 };
---
+++ CURSO DE JAVASCRIPT/AULA 16 SOBRE SETTIMEOUT E SETINTERVAL/arquivo.js
@@ -0,0 +1,12 @@
+/*  EVENTOS DE TEMPO NO JAVASCRIPT
+
+OS EVENTOS DE TEMPO NO JAVA SCRIPT PERMITEM A EXECUÇÃO DE UM CODIGO EM INTERVALOS DE TEMPO ESPECIFICADOS. ESSES INTERVALOS DE TEMPO SÃO CHAMADOS DE EVENTOS DE CRONOMETAGEM.
+
+Os  dois métodos-chave para usar com JavaScript são :
+
+setTimeout(function tempo em milsegundos)
+-> Executa uma função, depois de esperar um número especificado de milisegundos.
+
+setInterval(function, milisegundos)
+-> E o mesmo que o SetTimeou(), mas repete a execução da função continuamente .*/
+
