91a20679ccd9d7b3dc345479cad886828ed7b62d
Isaac <isaccgabriel110@gmail.com>
2024-06-18 19:46:30 -0300

    continuação da aula de JS sobre manipulação de datas

--- CURSO DE JAVASCRIPT/AULA 18 SOBRE MANIPULAÇÃO DE DATAS/arquivo.js
+++ CURSO DE JAVASCRIPT/AULA 18 SOBRE MANIPULAÇÃO DE DATAS/arquivo.js
@@ -10,10 +10,54 @@ ESTE COMANDO IRA MOSTRAR A DATA
 .
 
 AGORA VAMOS PARA A PARTE PRATICA. */
+let data = new Date();
+//PEGAR O ANO COM ESSE COMANDO
+let ano = data.getFullYear();
+console.log(ano);
 
+//COMANDO PARA PEGAR O MêS ATUAL SO QUE O 0 SERIA JANEIRO E O 11 DEZEMBRO
+let mes = data.getMonth();
+console.log(mes);
 
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho","Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ]
 
+let mesesNaTela = meses[data.getMonth()];
+console.log(mesesNaTela);
 
+//PEGAR DIA DO MêS 1 ATE 31
+
+let dia = data.getDate();
+console.log(dia);
+
+//PEGAR O DIA DA SEMANA,vai de 0 que seria domingo ate 6 que seria o sabado
+
+let semana = data.getDay();
+console.log(semana);
+
+const week = ["Domingo", "Segunda", "terça", "Quarta", "Quinta", "Sexta", "Sabado"]
+
+let diaDaSemana = week[data.getDay()];
+console.log(diaDaSemana);
+
+//AGORA PARA MOSTRAR A HORA 0 ATE 23
+
+let hora = data.getHours();
+console.log(hora);
+
+//PARA MOSTRAR OS MINUTOS QUE E DE 0 ATE 59
+
+let minuto = data.getMinutes();
+console.log(minuto);
+
+//PARA MOSTRAR  OS SEGUNDOS QUE TAMBEM VAI DE 0 A 59
+
+let segundos = data.getSeconds();
+console.log(segundos);
+
+//PARA MOSTRAR OS MILISEGUNDOS VAI DE 0 ATE 999
+
+let mili = data.getMilliseconds();
+console.log(mili);
 
 
 
