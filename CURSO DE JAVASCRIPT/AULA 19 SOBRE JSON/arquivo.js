3f82922e913eb06f544b9097e68cc52ee98a5db6
Isaac <isaccgabriel110@gmail.com>
2024-07-08 19:25:47 -0300

    ULTIMA AULA DE JS E FIM DA AULA SOBRE ALINHAMENTO DE EIXOS NO HTML E CSS

--- CURSO DE JAVASCRIPT/AULA 19 SOBRE JSON/arquivo.js
+++ CURSO DE JAVASCRIPT/AULA 19 SOBRE JSON/arquivo.js
@@ -5,13 +5,44 @@
 JSON.parse() ->  converte texto no padrão JSON para objetos.
 JSON.stringify() -> converte objetos  em texto padrão JSON . */
 
-const carro = {
-    marca: "Fiat",
-    ano: 2023,
-    modelo: "ARGO",
+/*  O COMANDO "XMLHttpRequest();"  E USADO PARA FAZER UMA REQUISIÇÃO DE UM SITE ISSO E PEGAR OU ACESSAR INFORMAÇÕES DE UM SITE DE FORMA REMOTA.
+
+E O "open('GET', URL DO SITE)" E USADO PARA ACESSAR A URL DO SITE, EXEMPLO DE COMANDO ABAIXO.
 
 
-};
+const ajax = new XMLHttpRequest();
 
-let texto = JSON.stringify(carro);
-document.getElementById('area').innerHTML = texto;
+ajax.open('GET', URL DO SITE);
+
+ajax.send();
+
+ajax.onload = function(){
+
+document.getElementById('id').innerHTML = this. responseText;
+
+}
+
+
+
+*/
+
+
+function buscaCEP() {
+    let  input = document.getElementById("cep").value;
+
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET','https://viacep.com.br/ws/' + input + '/json/');
+
+    ajax.send();
+
+    ajax.onload = function(){
+        document.getElementById('texto').innerHTML = this.responseText;
+}
+
+
+
+
+}
+
+
+
