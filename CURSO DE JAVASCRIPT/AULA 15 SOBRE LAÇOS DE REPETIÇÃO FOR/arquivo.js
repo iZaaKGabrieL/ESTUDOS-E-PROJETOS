5ca613e4bc993c464970fc7aad381e5b19e33bca
iZaaK <isaccgabriel00@gmail.com>
2025-03-04 15:06:54 -0300

    AULA DE JS

--- CURSO DE JAVASCRIPT/AULA 15 SOBRE LAÇOS DE REPETIÇÃO FOR/arquivo.js
+++
@@ -1,8 +0,0 @@
-/* OS LAÇOS DE REPETIÇÃO FOR SÃO USADOS PARA CRIAR AÇÕES QUE PRECISAM SER REPITIDAS MAIS DE UMA VEZ */
-
-
-var ano = new Date().getFullYear();
-
-for (let i = ano; i >= 1900; i--){
-    document.getElementById("ano").innerHTML += "<option value= '"+i+"'>"+i+"</option>";
-};
