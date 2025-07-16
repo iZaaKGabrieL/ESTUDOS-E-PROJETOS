1c8a4f6aec34a1c0e351ac811345078ba09b57e9
iZaaK <isaccgabriel00@gmail.com>
2025-07-16 18:42:19 -0300

    curso dejs do odin

---
+++ CURSO DE INTERMEDIARIO DE FRONT-END/CURSO JS/scripts.js
@@ -0,0 +1,16 @@
+function Player(name, marker){
+    this.name = name;
+    this.marker = marker;
+
+    this.sayname = function sayname(){
+    console.log(this.name);
+    console.log(`escolheu: ${this.marker}`);
+}
+
+};
+
+const jogador = new Player('isaac', '0');
+const jogador2 = new Player('Jarvis', 'X');
+
+jogador.sayname();
+jogador2.sayname();
