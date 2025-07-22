2d5bece0c3f83a0a15e2ee9d750edddd8fb41bf6
iZaaK <isaccgabriel00@gmail.com>
2025-07-22 14:43:50 -0300

    PROJETO QUASE PRONTO

--- CURSO DE INTERMEDIARIO DE FRONT-END/MINI-PROJETO LIBRARY/scripts.js
+++ CURSO DE INTERMEDIARIO DE FRONT-END/MINI-PROJETO LIBRARY/scripts.js
@@ -13,16 +13,18 @@ function Book (title, autor, category, pages, read) {
 }
 
 // button event
-const btn = document.getElementsByClassName("adicionarLivro");
+const btn = document.getElementsByClassName("adicionarLivro")[0];
  btn.addEventListener("click", (e) => {
     e.preventDefault();
     addBookLibrary();
  })
 
+  //PARA REMOVER OS LIVROS DA TELA  A OUTRA PARTE TA LA EMBAIXO NO FINAL
+ const botaoApagar = document.querySelector(".remover");
+
  // function to add Books for the dataBase (library)
 
 function addBookLibrary(){
-
     const titulo = document.getElementById("titulo").value;
     const autor = document.getElementById("autor").value;
     const genero = document.getElementById("genero").value;
@@ -35,16 +37,47 @@ function addBookLibrary(){
 
     console.log(library);
 
-}
-
+    
 // show the books in the screen:
 
- const tela = document.getElementsByClassName("estanteDeLivro")[0];
- tela.innerHTML = '';
+const tela = document.querySelector(".estanteDeLivro");
+
 library.forEach(livros => {
 
     const TituloDoLivro = document.createElement('h1');
     TituloDoLivro.textContent = livros.title;
+    const generoDolivro = document.createElement('h2');
+    generoDolivro.textContent = livros.category;
+    const AutorDolivro = document.createElement('h3');
+    AutorDolivro.textContent = livros.autor;
+    const PaginasDolivro = document.createElement('p');
+    PaginasDolivro.textContent = livros.pages;
+
+tela.innerHTML = " ";
+
 
 tela.appendChild(TituloDoLivro);
+tela.appendChild(generoDolivro);
+tela.appendChild(AutorDolivro);
+tela.appendChild(PaginasDolivro);
+
+ //PARA REMOVER OS LIVROS DA TELA II
+ 
+botaoApagar.addEventListener("click", (e) => {
+    e.preventDefault();
+    library.length = 0;
+    tela.innerHTML = '';
 });
+
+
+
+
+
+});
+
+
+
+
+
+
+}
