53e2e01acb3c684dcb830fba7bc8bef518d5f049
iZaaK <isaccgabriel00@gmail.com>
2025-07-24 16:35:18 -0300

    PROJETO TERMINADO COM SUCESSO

--- CURSO DE INTERMEDIARIO DE FRONT-END/MINI-PROJETO LIBRARY/scripts.js
+++ CURSO DE INTERMEDIARIO DE FRONT-END/MINI-PROJETO LIBRARY/scripts.js
@@ -31,18 +31,21 @@ function addBookLibrary(){
     const status = document.getElementById("status").value;
     const paginas = document.getElementById("paginas").value;
 
+console.log("Função foi chamada");
 
     const livros = new Book(titulo, autor, genero, paginas, status);
     library.push(livros);
 
-    console.log(library);
 
     
 // show the books in the screen:
 
 const tela = document.querySelector(".estanteDeLivro");
-
+tela.innerHTML = " ";
 library.forEach(livros => {
+
+    const livrocard = document.createElement('div');
+    livrocard.classList.add('livro')
 
     const TituloDoLivro = document.createElement('h1');
     TituloDoLivro.textContent = livros.title;
@@ -51,33 +54,25 @@ library.forEach(livros => {
     const AutorDolivro = document.createElement('h3');
     AutorDolivro.textContent = livros.autor;
     const PaginasDolivro = document.createElement('p');
-    PaginasDolivro.textContent = livros.pages;
-
-tela.innerHTML = " ";
+    PaginasDolivro.textContent = livros.pages
 
 
-tela.appendChild(TituloDoLivro);
-tela.appendChild(generoDolivro);
-tela.appendChild(AutorDolivro);
-tela.appendChild(PaginasDolivro);
+livrocard.appendChild(TituloDoLivro);
+livrocard.appendChild(generoDolivro);
+livrocard.appendChild(AutorDolivro);
+livrocard.appendChild(PaginasDolivro);
+
+tela.appendChild(livrocard);
+
+
+});
 
  //PARA REMOVER OS LIVROS DA TELA II
- 
-botaoApagar.addEventListener("click", (e) => {
+
+}
+
+ botaoApagar.addEventListener("click", (e) => {
     e.preventDefault();
     library.length = 0;
     tela.innerHTML = '';
 });
-
-
-
-
-
-});
-
-
-
-
-
-
-}
