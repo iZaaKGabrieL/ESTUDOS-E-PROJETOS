04abae68972f80f382a3e4508fe935a77ea9c584
iZaaK <isaccgabriel00@gmail.com>
2025-08-05 15:54:05 -0300

    FIM DA AULA SOBRE FABRICAS E MODULOS

--- CURSO DE INTERMEDIARIO DE FRONT-END/CURSO JS/Funções de Fábrica e o Padrão de Módulo/scripts.js
+++ CURSO DE INTERMEDIARIO DE FRONT-END/CURSO JS/Funções de Fábrica e o Padrão de Módulo/scripts.js
@@ -1,12 +1,39 @@
-function primeiro(num1){
-    const number1 = num1;
-    return function segundo(num2){
-        const number2 = num2;
-        return number1 + number2;
+function ContadorDeLike(){
+
+    let likes = 0;
+    return function() {
+        likes++
+        console.log("AGORA TEMOS UM TOTAL DE LIKES DE:", likes);
     }
- 
-   
+
 
 }
-    const add4 = primeiro(4);
-        console.log(add4(4));
+
+const curtidas = ContadorDeLike();
+
+curtidas();
+curtidas();
+curtidas();
+curtidas();
+curtidas();
+
+console.log("============================IGNORA TUDO ACIMA================================");
+
+const calculator = (function() {
+    //ABAIXO ESTA AS FUNÇÕES PRIVADAS QUE SERVIRAM PARA SEREM USADAS PARA CALCULOS E NAO ESTARAM EXPOSTAS.
+
+    const som = (a, b) => a + b;
+    const divis = (a, b) => a / b;
+    const subt = (a, b) => a - b;
+    const multip = (a, b) => a * b;
+
+    //APENAS OQUE ESTIVER DENTRO DO return SERA EXPOSTO E PODERAR SER USADO FORA DO MÓDULO
+    return {som, divis, subt, multip};
+
+})();
+
+//EXEMPLOS ABAIXO:
+console.log(calculator.som(10, 15));     // 25
+console.log(calculator.multip(3, 3));    // 9
+console.log(calculator.subt(80, 40));    // 40
+console.log(calculator.divis(10, 2));    // 5
